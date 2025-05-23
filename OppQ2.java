6d95a2d1e15172662e66fbcf86d23ef495e795a5
hassaanmalikgit <65963@students.riphah.edu.pk>
2025-05-23 10:14:33 +0500

    Add files via upload

---
+++ OppQ2.java
@@ -0,0 +1,15 @@
+public class OppQ2 {
+
+    public static void main(String[] args) {
+
+        scanner = new scanner (systen.ln);
+        system.out.println("Enter the number: ");
+
+        int num = sc.nextint ();
+        int cube = num * num * num;
+
+        System.out.println("cube of the number is +num is +cube");
+
+            }
+        }
+
